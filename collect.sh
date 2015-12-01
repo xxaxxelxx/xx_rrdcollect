da3d59a5afd182e5d18f4dbea9a9a9333dd4f905
xxaxxelxx <x@axxel.net>
2015-12-01 15:30:49 +0100

    updated

--- collect.sh
+++ collect.sh
@@ -9,7 +9,7 @@ test -z $CUSTOMER && exit;
 while true; do
     OIFS=$IFS; IFS=$'\n'; A_MOUNTPOINTS=($(curl --connect-timeout 3 -s "http://$LB_HOST/listmountpoints.php?mnt=$CUSTOMER")); IFS=$OIFS
     for C_MNT in ${A_MOUNTPOINTS[@]}; do
-	C_VALUE=$(curl --connect-timeout 3 -s "http://$LB_HOST/listeners.php?mnt=$CUSTOMER")
+	C_VALUE=$(curl --connect-timeout 3 -s "http://$LB_HOST/listeners.php?mnt=$C_MNT")
 	echo "$C_MNT is $C_VALUE"
     done
     sleep $RRD_LOOP
