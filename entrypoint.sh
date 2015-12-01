86646e7094a140e849ad9218534364739784d1ab
xxaxxelxx <x@axxel.net>
2015-12-01 14:24:30 +0100

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ LINKED_CONTAINER=$(env | grep '_ENV_' | head -n 1 | awk '{print $1}' | sed 's/_E
 LB_HOST="$(cat /etc/hosts | grep -iw ${LINKED_CONTAINER} | awk '{print $1}')"
 #eval LB_PORT=\$${LINKED_CONTAINER}_ENV_IC_PORT
 
-./collect.sh $LB_HOST $RRD_LOOP $CUSTOMER
-#bash
+#./collect.sh $LB_HOST $RRD_LOOP $CUSTOMER
+bash
 exit
