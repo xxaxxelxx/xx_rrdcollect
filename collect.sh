dd8a7fa50d7b47f651489e12502293b363ba1b34
xxaxxelxx <x@axxel.net>
2015-12-01 16:06:07 +0100

    updated

--- collect.sh
+++ collect.sh
@@ -8,10 +8,23 @@ test -z $CUSTOMER && exit;
 
 while true; do
     OIFS=$IFS; IFS=$'\n'; A_MOUNTPOINTS=($(curl --connect-timeout 3 -s "http://$LB_HOST/listmountpoints.php?mnt=$CUSTOMER")); IFS=$OIFS
-    for C_MNT in ${A_MOUNTPOINTS[@]}; do
-	C_VALUE=$(curl --connect-timeout 3 -s "http://$LB_HOST/listeners.php?mnt=$C_MNT")
-	echo "$C_MNT is $C_VALUE"
+    for MNT in ${A_MOUNTPOINTS[@]}; do
+	C_VALUE=$(curl --connect-timeout 3 -s "http://$LB_HOST/listeners.php?mnt=$MNT")
+	echo "$MNT is $C_VALUE"
+	C_MNT=$(echo $MNT | sed 's|^/||')
+	RRDFILE="/customer/$CUSTOMER/_$C_MNT.rrd"
+	test -f $RRDFILE || (
+	    rrdtool create $RRDFILE \
+	    --step $RRD_LOOP \
+	    DS:$C_MNT:GAUGE:$(($RRD_LOOP*2)):U:U \
+	    RRA:MAX:0.5:1:$(( $((3600 / $RRD_LOOP )) * 24 )) \
+	    RRA:MAX:0.5:4:$(( $(( $(( 3600 / $(( $RRD_LOOP * 4 ))  )) * 24 )) * 7 )) \
+	    RRA:MAX:0.5:12:$(( $(( $(( 3600 / $(( $RRD_LOOP * 12 ))  )) * 24 )) * 31 )) \
+	    RRA:MAX:0.5:100:$(( $(( $(( 3600 / $(( $RRD_LOOP * 100 ))  )) * 24 )) * 366 ))
+	)
+	rrdtool update $RRDFILE N:$C_VALUE
     done
+
     sleep $RRD_LOOP
 done
 
