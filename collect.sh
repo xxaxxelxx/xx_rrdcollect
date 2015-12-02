c3383b836a14491fd789e5bf088fcbdbe248525d
xxaxxelxx <x@axxel.net>
2015-12-02 10:27:22 +0100

    updated

--- collect.sh
+++ collect.sh
@@ -11,27 +11,28 @@ if [ "x$CUSTOMER" == "xadmin" ]; then
 	OIFS=$IFS; IFS=$'\n'; A_MACHINES=($(curl --connect-timeout 3 -s "http://$LB_HOST/listmachines.php")); IFS=$OIFS
 	for MACHINE in ${A_MACHINES[@]}; do
 	    OIFS=$IFS; IFS='|'; A_MACHINE_DATA=($(echo "$MACHINE")); IFS=$OIFS
-	    C_IP=${A_MACHINE_DATA[0]}
+	    IP=${A_MACHINE_DATA[0]}
+	    C_IP=$(echo $IP | sed 's|\.|\-|g')
 	    C_BW=${A_MACHINE_DATA[1]}
 	    C_BWLOAD=${A_MACHINE_DATA[2]}
 	    C_LOAD=${A_MACHINE_DATA[3]}
 	    RRDFILE="/customer/$CUSTOMER/_$C_IP.rrd"
-	    echo "$C_IP $C_BW $C_BWLOAD $C_LOAD" >> $RRDFILE
-#	    C_MNT=$(echo $MNT | sed 's|^/||' | sed 's|\.|\_|g')
-#	    test -f $RRDFILE || (
-#		ITEMS_DAY=$(( $((2 * 24 * 60 * 60)) / $(( $RRD_LOOP * 1 )) ))
-#		ITEMS_WEEK=$(( $((8 * 24 * 60 * 60)) / $(( $RRD_LOOP * 4 )) ))
-#		ITEMS_MONTH=$(( $((32 * 24 * 60 * 60)) / $(( $RRD_LOOP * 12 )) ))
-#		ITEMS_YEAR=$(( $((366 * 24 * 60 * 60)) / $(( $RRD_LOOP * 100 )) ))
-#		rrdtool create $RRDFILE \
-#		--step $RRD_LOOP \
-#		DS:$C_MNT:GAUGE:$(($RRD_LOOP*2)):U:U \
-#		RRA:MAX:0.5:1:$ITEMS_DAY \
-#		RRA:MAX:0.5:4:$ITEMS_WEEK \
-#		RRA:MAX:0.5:12:$ITEMS_MONTH \
-#		RRA:MAX:0.5:100:$ITEMS_YEAR
-#	    )
-#	    rrdtool update $RRDFILE N:$C_VALUE
+	    test -f $RRDFILE || (
+		ITEMS_DAY=$(( $((2 * 24 * 60 * 60)) / $(( $RRD_LOOP * 1 )) ))
+		ITEMS_WEEK=$(( $((8 * 24 * 60 * 60)) / $(( $RRD_LOOP * 4 )) ))
+		ITEMS_MONTH=$(( $((32 * 24 * 60 * 60)) / $(( $RRD_LOOP * 12 )) ))
+		ITEMS_YEAR=$(( $((366 * 24 * 60 * 60)) / $(( $RRD_LOOP * 100 )) ))
+		rrdtool create $RRDFILE \
+		--step $RRD_LOOP \
+		DS:bw:GAUGE:$(($RRD_LOOP*2)):U:U \
+		DS:bwlimit:GAUGE:$(($RRD_LOOP*2)):U:U \
+		DS:cpuload:GAUGE:$(($RRD_LOOP*2)):U:U \
+		RRA:MAX:0.5:1:$ITEMS_DAY \
+		RRA:MAX:0.5:4:$ITEMS_WEEK \
+		RRA:MAX:0.5:12:$ITEMS_MONTH \
+		RRA:MAX:0.5:100:$ITEMS_YEAR
+	    )
+	    rrdtool update $RRDFILE N:$C_BW:$C_BWLOAD:$C_LOAD
 	done
 	sleep $RRD_LOOP
     done
